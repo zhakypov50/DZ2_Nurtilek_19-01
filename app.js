e6bcdc4c5a6007479871d199b8109ec8efdfa7fa
zhakypov50 <115090188+zhakypov50@users.noreply.github.com>
2022-10-08 10:14:54 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//столбец чисел 1-50
+let i = 1;
+while (i < 51) {
+    console.log(i);
+    i++;
+}
+
+//фраза 'Привет, мир!' через console
+let text = ['Привет, ', 'мир', '!'];
+console.log(text[0] + text[1] + text[2]);
+
+//фраза 'Привет, мир!' через alert
+let text2 = ['Привет, ', 'мир', '!'];
+alert(text2[0] + text2[1] + text[2]);
